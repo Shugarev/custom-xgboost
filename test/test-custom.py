34f4b83ef703aee5b139ff605ace7b7ebe1c0d78
Timur Taran <timataran@gmail.com>
2017-11-06 00:31:24 +0200

    Some initial changes

---
+++ test/test-custom.py
@@ -0,0 +1,15 @@
+import unittest
+import pandas as pd
+from custom_classifier import CustomClassifier
+
+
+def test_classifier():
+  df = pd.read_csv("/home/timur/Work/Sergey/custom-xgboost/data/merchant-11/train-custom-xgboost.csv")
+  cc = CustomClassifier()
+  cc.fit(df, 'status')
+
+
+if __name__ == '__main__':
+  unittest.main()
+
+
